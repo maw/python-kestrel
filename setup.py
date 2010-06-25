e8aa239a839eec96deeddf574dbe878a58215644
Michael Wolf <maw@pobox.com>
2010-06-25 13:20:23 -0500

    first cut at packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(name="kestrel",
+      version="0.1",
+      description="Oversimple library to talk to Kestrel",
+      author="Michael Wolf",
+      author_email="maw+python-kestrel@pobox.com",
+      keywords="queues kestrel memcache",
+      py_modules=["__init__", "kestrel"])
+
