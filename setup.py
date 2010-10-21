fad15c908c557703b00c9aff12fdaf1df2d18fab
Michael Wolf <maw@pobox.com>
2010-10-21 11:02:46 -0500

    bump version to 0.1.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name="kestrel",
-      version="0.1.7",
+      version="0.1.8",
       description="Oversimple library to talk to Kestrel",
       author="Michael Wolf",
       author_email="maw+python-kestrel@pobox.com",
